99fb6d07beb786ff6193ebe5bb7f06026d697a01
bwhite2 <bwhite2@iastate.edu>
2019-09-12 17:09:17 -0500

    Updated endpoints; Created base endpoints with regurgitation

---
+++ BackEnd/CydeNSeek/src/main/java/model/GameUser.java
@@ -0,0 +1,24 @@
+package model;
+
+public class GameUser {
+
+	private String session;
+
+	private String location;
+
+	public String getSession() {
+		return session;
+	}
+
+	public void setSession(String session) {
+		this.session = session;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
