2d4a3769cdd560b97264d45466158b6b6966052f
asmartin <asmartin@iastate.edu>
2019-09-29 22:20:00 -0500

    functions to fill textview with json

--- FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/PlayerListFragment.java
+++ FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/PlayerListFragment.java
@@ -1,18 +1,74 @@
 package com.example.wjmas_000.menu;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class PlayerListFragment extends Fragment {
 
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mTextViewResult = (TextView) getView().findViewById(R.id.text_view_result);
+        Button buttonParse = (Button) getView().findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_player_list, container, false);
+
+        private void jsonParse(){
+            String url = "http://coms-309-vb-1.misc.iastate.edu:8080/users";
+            //maybe pass json array in the future?
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("users");
+                        //iterates through all users in the json array
+                        for (int i =0; i < jsonArray.length(); i++){
+                            JSONObject user = jsonArray.getJSONObject(i);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            });
+        }
+
     }
 }
