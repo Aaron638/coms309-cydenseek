d0732853f64281b6551fded2b63458ce0a92c8a8
Cody Tomkins <ctomkins@iastate.edu>
2019-10-04 18:04:16 -0500

    Tests for miscController

--- BackEnd/CydeNSeek/src/test/java/application/controller/MiscControllerTest.java
+++ BackEnd/CydeNSeek/src/test/java/application/controller/MiscControllerTest.java
@@ -1,27 +1,60 @@
 package application.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import application.db.GameDB;
+import application.db.UserDB;
+import application.model.User;
+import static org.hamcrest.Matchers.containsString;
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class MiscControllerTest {
+	
+	@Mock
+	private GameDB gdb;
+	
+	@Mock
+	private UserDB udb;
+	
+	private List<User> users;
 
 	@InjectMocks
 	private MiscController miscController = new MiscController();
 
 	private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(miscController).build();
-
+	
+	@Before
+	public void setup()
+	{	
+		users = new ArrayList<User>();
+		User u = new User();
+		u.setUsername("John");
+		u.setSession("abc");
+		u.setGphider(12);
+		u.setGpseeker(10);
+		u.setGwhider(5);
+		u.setGwseeker(8);
+		users.add(u);
+	}
 	@Ignore
 	@Test
 	public void index() throws Exception {
@@ -38,17 +71,19 @@ public class MiscControllerTest {
 			.andExpect(content().string(""));
 	}
 
-	@Ignore
 	@Test
 	public void leaderboard() throws Exception {
+		when(udb.findAllUsersSorted(any(Comparator.class))).thenReturn(users);		
 		this.mockMvc.perform(get("/leaderboard"))
 			.andExpect(status().isOk())
-			.andExpect(content().string(""));
+			.andExpect(content().string(containsString("John")))
+			.andExpect(content().string(containsString("abc")));
 	}
 
 	@Ignore
 	@Test
 	public void users() throws Exception {
+		when(udb.findAllUsersSorted(any(Comparator.class))).thenReturn(users);
 		this.mockMvc.perform(get("/users"))
 			.andExpect(status().isOk())
 			.andExpect(content().string(""));
