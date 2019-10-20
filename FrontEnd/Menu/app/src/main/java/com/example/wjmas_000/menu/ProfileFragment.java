b98eb513ea507d56ce32cc1aaf9d7e9a260afdf3
asmartin <asmartin@iastate.edu>
2019-10-20 17:33:10 -0500

    new app icon by Aaron Martin

--- FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/ProfileFragment.java
+++ FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/ProfileFragment.java
@@ -13,6 +13,8 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        
         return inflater.inflate(R.layout.fragment_profile, container, false);
 }
 }
