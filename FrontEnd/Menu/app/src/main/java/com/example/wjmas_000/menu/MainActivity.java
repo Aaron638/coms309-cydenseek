2f08352ff3a63f47c2999609006f469196e1868a
asmartin <asmartin@iastate.edu>
2019-10-20 18:52:21 -0500

    Revert "attempt at web sockets and location data retrieval"
    This reverts commit da61505dfb0b2b80e5b46b93442d329f56aab4cd.
    
    Changes to main MainActivity crashes the map screen. Will work on later

--- FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/MainActivity.java
+++ FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/MainActivity.java
@@ -1,14 +1,7 @@
 package com.example.wjmas_000.menu;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
-import android.provider.Telephony;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,28 +9,11 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.handshake.ServerHandshake;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
-    private WebSocketClient cc;
-    private double longitude;
-    private double lattitude;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,77 +40,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             navigationView.setCheckedItem(R.id.nav_message);
         }
         */
-
-        //This is for location
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        longitude = location.getLongitude();
-        lattitude = location.getLatitude();
-
-        //String ses = "session";
-        String lonStr = Double.toString(longitude);
-        String latStr = Double.toString(lattitude);
-
-        Draft[] drafts = {new Draft_6455()};
-
-        String w = "ws://coms-309-vb-1.misc.iastate.edu:8080/user/{username}/location/";
-
-        try{
-            Log.d("Socket:", "Trying socket");
-            cc = new WebSocketClient(new URI(w), (Draft) drafts[0]){
-                @Override
-                public void onOpen(ServerHandshake handshake) {
-                    Log.d("OPEN", "run() returned: " + "is connecting");
-                }
-
-                @Override
-                public void onMessage(String message) {
-                    Log.d("", "run() returned: "+ message);
-                    //???? what goes here?
-                }
-
-                @Override
-                public void onClose(int code, String reason, boolean remote) {
-                    Log.d("CLOSE", "onClose() returned: " + reason);
-                }
-
-
-                @Override
-                public void onError(Exception e)
-                {
-                    Log.d("Exception:", e.toString());
-                }
-
-            };
-        } catch (URISyntaxException e) {
-            Log.d("Exception:", e.getMessage().toString());
-            e.printStackTrace();
-        }
-        cc.connect();
-
-        try{
-            cc.send("{\n" +
-                    "    \"session\": \"abc-123-xyz\",\n" +
-                    "    \"latitude\":" + lonStr + " ,\n" +
-                    "    \"longitude\"" + latStr + " ,\n" +
-                    "}");
-        } catch (Exception e)
-        {
-            Log.d("ExceptionSendMessage:", e.getMessage().toString());
-        }
-
-
-
     }
 
     @Override
