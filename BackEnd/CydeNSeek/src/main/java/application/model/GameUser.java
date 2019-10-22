d3cad5cd7c9575708cd7de122e3d5a8049da0560
bwhite2 <bwhite2@iastate.edu>
2019-10-21 19:59:14 -0500

    Added primary key to GameUser

--- BackEnd/CydeNSeek/src/main/java/application/model/GameUser.java
+++ BackEnd/CydeNSeek/src/main/java/application/model/GameUser.java
@@ -4,72 +4,72 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
 public class GameUser {
-	
+
 	@Column
 	@ManyToOne
 	@JoinColumn(name = "gameuser")
 	private General general;
-	
+
 	@Column
 	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "GU")
 	private Game game;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Integer id;
+
 	@Column
 	private Boolean found;
-	
+
 	@Column
 	private Boolean isHider;
-	
+
 	@Column
 	private Double latitude;
-	
+
 	@Column
 	private Double longitude;
-	
-	public Boolean getFound()
-	{
+
+	public Boolean getFound() {
 		return found;
 	}
-	
-	public void setFound(Boolean found)
-	{
-		this.found=found;
+
+	public void setFound(Boolean found) {
+		this.found = found;
 	}
-	
-	public Boolean getIsHider()
-	{
+
+	public Boolean getIsHider() {
 		return isHider;
 	}
-	
-	public void setIsHider(Boolean isHider)
-	{
-		this.isHider=isHider;
+
+	public void setIsHider(Boolean isHider) {
+		this.isHider = isHider;
 	}
-	
-	public Double getLatitude()
-	{
+
+	public Double getLatitude() {
 		return latitude;
 	}
-	
-	public void setLatitude(Double latitude)
-	{
-		this.latitude=latitude;
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
 	}
-	
-	public Double getLongitude()
-	{
+
+	public Double getLongitude() {
 		return longitude;
 	}
-	
-	public void setLongitude(Double longitude)
-	{
-		this.longitude=longitude;
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
 	}
 
 	public Game getGame() {
