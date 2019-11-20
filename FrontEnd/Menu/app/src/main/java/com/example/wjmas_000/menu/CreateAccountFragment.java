8bb426ae5c00dc7dfc945aa101954d7f1c3db96a
wjmaston <wjmaston@iastate.edu>
2019-11-19 21:24:32 -0600

    Create account seems to work, servers down so im not sure though.  If this works in the morning then we are in the clear for login and logout

--- FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/CreateAccountFragment.java
+++ FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/CreateAccountFragment.java
@@ -3,12 +3,26 @@ package com.example.wjmas_000.menu;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.android.volley.VolleyLog.TAG;
 
 
 public class CreateAccountFragment extends Fragment {
@@ -19,6 +33,8 @@ public class CreateAccountFragment extends Fragment {
     private EditText passwordConfirm;
     private Button createBtn;
     private TextView result;
+    private RequestQueue mQueue;
+    private String session = null;
 
     @Nullable
     @Override
@@ -52,23 +68,29 @@ public class CreateAccountFragment extends Fragment {
         //0: Check if both passwords match
         if(pass.equals(passConfirm)){
             //1: send username and password strings to backend
+            String url = "http://coms-309-vb-1.misc.iastate.edu:8080/user/";
+            url = url + user;
+            mQueue = Volley.newRequestQueue(getActivity());
+
+            JSONObject json = new JSONObject();
+            try {
+                json.put("password",pass);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            jsonSend(url, json);
+
+
+
 
 
             //2.0: get response from backend, Confirming that this combination is allowed
-
-
             //2.1: If user gave viable information, then display "Successful Login". Set activity variable 'LoginCode' to key given by backend
-
-
-            //2.2: If user did not give correct information, then display "Failed Login".  Do not move from this fragment
-
-
-
-
-            if((user.equals("James")) && pass.equals("Bond")){
+            if(session != null){
                 hold = "Account Created";
                 result.setText(hold);
             }
+            //2.2: If user did not give correct information, then display "Failed Login".  Do not move from this fragment
             else{
                 hold = "Username already in use";
                 result.setText(hold);
@@ -85,4 +107,38 @@ public class CreateAccountFragment extends Fragment {
 
 
     }
+
+    public void jsonSend(String url, JSONObject json) {
+
+
+
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, json, new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+                try {
+                    JSONObject auth = response.getJSONObject("session");
+                    //iterates through all users in the json array
+                        String ses = auth.getString("session");
+                        session = ses;
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: error");
+                error.printStackTrace();
+                //mTextViewResult.setText("Error");
+            }
+        });
+
+        mQueue.add(request);
+    }
+
+
+
 }
