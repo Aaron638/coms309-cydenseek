95b0e16dbc43f1c3403df48fdd87e08b70e0b126
asmartin <asmartin@iastate.edu>
2019-12-09 01:42:51 -0600

    join and create game refactor complete

--- FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/ClickListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.wjmas_000.menu;
-
-public interface ClickListener {
-    void onPositionClicked(int position);
-
-}
--- FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/JoinFragment.java
+++ FrontEnd/Menu/app/src/main/java/com/example/wjmas_000/menu/JoinFragment.java
@@ -1,20 +1,14 @@
 package com.example.wjmas_000.menu;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.SeekBar;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,89 +23,121 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 
 import static com.android.volley.VolleyLog.TAG;
 
 
 public class JoinFragment extends Fragment {
 
-    private RequestQueue mQueue;
-    JSONArray gamesJsonArray;
-    ArrayList<String> gameUserNames;
-    ArrayList<String> gameMaxPlayers;
-    RecyclerView recyclerView;
+    private RequestQueue rq;
     String username;
     String userSession;
-    TextView noGames;
+    TextView gameListTextView;
+    String creatorUsername;
+    HashMap<String, String> gamesList;
+    EditText creatorNameEditText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_join, container, false);
 
+        View rootView = inflater.inflate(R.layout.fragment_join, container, false);
         username = ((MenuActivity) getActivity()).getUsername();
         userSession = ((MenuActivity) getActivity()).getSession();
+        rq = Volley.newRequestQueue(getActivity());
 
-        gameUserNames = new ArrayList<>();
-        gameMaxPlayers = new ArrayList();
+        gameListTextView = (TextView) rootView.findViewById(R.id.text_game_list);
+        creatorNameEditText = (EditText) rootView.findViewById(R.id.edit_creator_user_name);
 
-        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
 
-        noGames = (TextView) rootView.findViewById(R.id.text_noGames);
-        //areGames = noGames.getText().toString();
+        //usernames, sessions
+        gamesList = new HashMap<String, String>();
 
-        Button buttonRefresh = (Button) rootView.findViewById(R.id.button_refresh);
-        mQueue = Volley.newRequestQueue(getActivity());
-
-        buttonRefresh.setOnClickListener(new View.OnClickListener() {
+        //When refreshing, we should see the list of games from the GET /games Volley Request
+        Button refreshButton = (Button) rootView.findViewById(R.id.button_refresh);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                callBackend();
+                gameListTextView.setText("");
+                gamesList.clear();
+                backendCallGames();
             }
-
-
         });
 
+        //When we press this button, we check the editText to see if that creator actually made a game
+        //Then we launch the game
+        Button joinGameButton = (Button) rootView.findViewById(R.id.button_join);
+        joinGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                creatorUsername = creatorNameEditText.getText().toString();
+                if (gamesList.containsKey(creatorUsername)){
+                    launchGame(gamesList.get(creatorUsername));
+                } else {
+                    Toast.makeText(getContext(), "Incorrect username", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         return rootView;
     }
 
-    private void callBackend() {
+    //launches the game activity
+    private void launchGame(String gameSession){
+        Intent intent = new Intent(getActivity(), GameActivity.class);
+        intent.putExtra("GAME_SESSION_ID", gameSession);
+        startActivity(intent);
+    }
 
-        String urlGames = "http://coms-309-vb-1.misc.iastate.edu:8080/games/";
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, urlGames, null, new Response.Listener<JSONObject>() {
+    //Use Volley to send a GET request to /games
+    private void backendCallGames() {
+        String uri = "http://coms-309-vb-1.misc.iastate.edu:8080/games";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, uri, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 Log.d(TAG, response.toString());
                 try {
-                    gamesJsonArray = response.getJSONArray("games");
+                    JSONArray jsonArray = response.getJSONArray("games");
 
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject game = jsonArray.getJSONObject(i);
+
+                        String creator = game.getString("creator");
+                        String session = game.getString("session");
+                        gamesList.put(creator, session);
+
+                        String result = "Creator: " + creator +
+                                "\nGame Session: " + gamesList.get(creator) +
+                                "\n\n";
+
+                        gameListTextView.append(result);
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
+
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.d(TAG, "onErrorResponse: error");
                 error.printStackTrace();
-                //mTextViewResult.setText("Error");
+                gameListTextView.setText("Error");
             }
-        });
-        mQueue.add(request);
-
-
+        }
+                //end JsonObjectRequest
+        );
+        rq.add(request);
     }
 
 
